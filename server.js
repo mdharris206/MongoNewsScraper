69aa0f70901b4d6335cc32206689138f9ee31cc5
mdh206 <49321381+mdharris206@users.noreply.github.com>
2019-06-28 09:31:21 -0400

    process.env

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/MNS_db", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/MNS_db";
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true });
 
 //Routes
 //A GET rout scraping GPB
